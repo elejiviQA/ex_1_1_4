d3bc25c965a67a27c0889ed9f45264a30b0a890b
Vladimir Voblikov <vladimir.voblikov.774@gmail.com>
2023-04-05 00:12:30 +0500

    #Add manual transaction management (TIRED)

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -5,6 +5,7 @@ import jm.task.core.jdbc.util.Util;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Savepoint;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +17,6 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void createUsersTable() {
 
-        Connection connection = null;
         String createUsersTable = """
                 CREATE TABLE IF NOT EXISTS users(
                 id INT PRIMARY KEY AUTO_INCREMENT,
@@ -26,52 +26,39 @@ public class UserDaoJDBCImpl implements UserDao {
                 );
                 """;
 
-        try {
-            connection = Util.get();
+        try (var connection = Util.get()) {
             connection.setAutoCommit(false);
-            var statement = connection.createStatement();
-            statement.executeUpdate(createUsersTable);
-            connection.commit();
-            connection.setAutoCommit(true);
-        } catch (SQLException e) {
-            // try { connection.rollback(); } catch (SQLException ex) { throw new RuntimeException(ex); } //rollback for DDL?
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
+            try (var statement = connection.createStatement()) {
+                statement.executeUpdate(createUsersTable);
+                connection.commit();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
+            } finally {
+                connection.setAutoCommit(true);
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public void dropUsersTable() {
 
-        Connection connection = null;
         String dropUsersTable = """
                 DROP TABLE IF EXISTS users;
                 """;
 
-        try {
-            connection = Util.get();
+        try (var connection = Util.get()) {
             connection.setAutoCommit(false);
-            var statement = connection.createStatement();
-            statement.executeUpdate(dropUsersTable);
-            connection.commit();
-            connection.setAutoCommit(true);
-        } catch (SQLException e) {
-            // try { connection.rollback(); } catch (SQLException ex) { throw new RuntimeException(ex); } //rollback for DDL?
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
+            try (var statement = connection.createStatement()) {
+                statement.executeUpdate(dropUsersTable);
+                connection.commit();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
+            } finally {
+                connection.setAutoCommit(true);
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -79,61 +66,43 @@ public class UserDaoJDBCImpl implements UserDao {
 
         String saveUser = String.format("INSERT INTO users (name, lastName, age) VALUES ('%s', '%s', '%d');", name, lastName, age);
         String result = String.format("User с именем – '%s' добавлен в базу данных", name);
-        Connection connection = null;
 
-        try {
-            connection = Util.get();
+        try (var connection = Util.get()) {
             connection.setAutoCommit(false);
-            var statement = connection.createStatement();
-            statement.executeUpdate(saveUser);
-            connection.commit();
-            connection.setAutoCommit(true);
-            System.out.println(result);
-        } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
+            try (var statement = connection.createStatement()) {
+                statement.executeUpdate(saveUser);
+                connection.commit();
+                System.out.println(result);
             } catch (SQLException e) {
+                connection.rollback();
+                System.out.println("-> rollback");
                 throw new RuntimeException(e);
+            } finally {
+                connection.setAutoCommit(true);
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public void removeUserById(long id) {
 
         String removeUserById = String.format("DELETE FROM users WHERE id = '%d'", id);
-        Connection connection = null;
 
-        try {
-            connection = Util.get();
+        try (var connection = Util.get()) {
             connection.setAutoCommit(false);
-            var statement = connection.createStatement();
-            statement.executeUpdate(removeUserById);
-            connection.commit();
-            connection.setAutoCommit(true);
-        } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
+            try (var statement = connection.createStatement()) {
+                statement.executeUpdate(removeUserById);
+                connection.commit();
             } catch (SQLException e) {
+                connection.rollback();
+                System.out.println("-> rollback");
                 throw new RuntimeException(e);
+            } finally {
+                connection.setAutoCommit(true);
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -141,12 +110,9 @@ public class UserDaoJDBCImpl implements UserDao {
 
         String getAllUsers = "SELECT * FROM users";
         List<User> users = new ArrayList<>();
-        Connection connection = null;
 
-        try {
-            connection = Util.get();
-            connection.setAutoCommit(false);
-            var statement = connection.createStatement();
+        try (var connection = Util.get();
+            var statement = connection.createStatement()) {
             var resultExecute = statement.executeQuery(getAllUsers);
             while (resultExecute.next()) {
                 User user = new User();
@@ -156,56 +122,30 @@ public class UserDaoJDBCImpl implements UserDao {
                 user.setAge(resultExecute.getByte("age"));
                 users.add(user);
             }
-
-            connection.commit();
-            connection.setAutoCommit(true);
         } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
         }
-
         return users;
     }
 
     public void cleanUsersTable() {
 
         String cleanUsersTable = "DELETE FROM live.users;";
-        Connection connection = null;
 
-        try {
-            connection = Util.get();
+        try (var connection = Util.get()) {
             connection.setAutoCommit(false);
-            var statement = connection.createStatement();
-            statement.executeUpdate(cleanUsersTable);
-            connection.commit();
-            connection.setAutoCommit(true);
-        } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
+            try (var statement = connection.createStatement()) {
+                statement.executeUpdate(cleanUsersTable);
+                connection.commit();
             } catch (SQLException e) {
+                connection.rollback();
+                System.out.println("-> rollback");
                 throw new RuntimeException(e);
+            } finally {
+                connection.setAutoCommit(true);
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
